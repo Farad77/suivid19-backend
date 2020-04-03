57ba5e94d0c88882cd829ad0c42da57271ca802e
unknown <maxime.arequiom@gmail.com>
2020-04-03 12:10:29 +0400

    push heroku jwt a2

--- src/app/patient/patients/patients.component.ts
+++ src/app/patient/patients/patients.component.ts
@@ -1,26 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
-
 @Component({
   selector: 'app-patients',
   templateUrl: './patients.component.html',
@@ -28,20 +8,16 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class PatientsComponent implements OnInit {
 
-  private _urlPersonnes = 'https://suivid19-api.herokuapp.com/patients';
+  private _urlPatients = 'https://suivid19-api.herokuapp.com/patients';
   patients:any[];
 
   constructor(private _httpClient: HttpClient) {
   }
 
   ngOnInit() {
-    this._httpClient.get(this._urlPersonnes)
+    this._httpClient.get(this._urlPatients)
       .subscribe((res:any) => {
         this.patients = res;
       });
   }
-
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
-
 }
