94017090e16e22766fdf754c33b3e230716f40a5
unknown <maxime.arequiom@gmail.com>
2020-04-03 09:23:21 +0400

    component login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SymptomComponent } from './symptom/symptom.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { PatientsComponent } from './patient/patients/patients.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { PatientsComponent } from './patient/patients/patients.component';
     TrackingComponent,
     TestCompletionsComponent,
     SymptomComponent,
-    PatientsComponent
+    PatientsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
