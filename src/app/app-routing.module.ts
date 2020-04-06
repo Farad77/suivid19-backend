dc2e85cf2310c15b0d98235840c1a75b851383a6
Romain <gautromain974@gmail.com>
2020-04-06 14:51:56 +0400

    Add Stats routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PatientsComponent } from './patient/patients/patients.component';
+import { StatsComponent } from './patient/stats/stats.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuardService } from './services/auth-guard.service';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'patient/patients', component: PatientsComponent, canActivate: [AuthGuardService] }];
+  { path: 'patient/patients', component: PatientsComponent, canActivate: [AuthGuardService] },
+  { path: 'patient/stats', component: StatsComponent, canActivate: [AuthGuardService] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
