2a28c6f2c682aeb7a6fceaf676a4ab41fddc655c
Romain <gautromain974@gmail.com>
2020-04-10 15:36:43 +0400

    Bug

--- src/app/doctor/stats/patient/patient.component.ts
+++ src/app/doctor/stats/patient/patient.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-patient',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient.component.css']
 })
 export class PatientComponent implements OnInit {
+  private _urlPatient: string;
+  id: number;
+  monPatient: any;
 
-  constructor() { }
+  constructor(private _httpClient: HttpClient, private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+   this.id = this.activatedRoute.snapshot.params.id;
+    this._urlPatient = 'https://suivid19-api.herokuapp.com/patients/' + this.id;
+
+
+    this._httpClient.get(this._urlPatient)
+      .subscribe((res: any) => {
+        this.monPatient = res;
+        console.log(this.monPatient);
+      });
   }
 
 }
